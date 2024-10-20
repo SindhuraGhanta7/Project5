7b995dbdc5ba9ff380c5af01e66c621214530722
Aakash Vidiyala <avidiya1@charlotte.edu>
2024-10-20 03:32:01 -0400

    BugFixes and Updates in the code.

--- webServer.js
+++ webServer.js
@@ -1,73 +1,267 @@
-import React from 'react';
-import {
-  List,
-  ListItemText,
-  ListItemButton,
-} from '@mui/material';
-import './userList.css';
-import axios from 'axios';
+/**
+ * This builds on the webServer of previous projects in that it exports the
+ * current directory via webserver listing on a hard code (see portno below)
+ * port. It also establishes a connection to the MongoDB named 'project6'.
+ *
+ * To start the webserver run the command:
+ *    node webServer.js
+ *
+ * Note that anyone able to connect to localhost:portNo will be able to fetch
+ * any file accessible to the current user in the current directory or any of
+ * its children.
+ *
+ * This webServer exports the following URLs:
+ * /            - Returns a text status message. Good for testing web server
+ *                running.
+ * /test        - Returns the SchemaInfo object of the database in JSON format.
+ *                This is good for testing connectivity with MongoDB.
+ * /test/info   - Same as /test.
+ * /test/counts - Returns the population counts of the cs collections in the
+ *                database. Format is a JSON object with properties being the
+ *                collection name and the values being the counts.
+ *
+ * The following URLs need to be changed to fetch their reply values from the
+ * database:
+ * /user/list         - Returns an array containing all the User objects from
+ *                      the database (JSON format).
+ * /user/:id          - Returns the User object with the _id of id (JSON
+ *                      format).
+ * /photosOfUser/:id  - Returns an array with all the photos of the User (id).
+ *                      Each photo should have all the Comments on the Photo
+ *                      (JSON format).
+ */
 
-class UserList extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      users: [],
-    };
+const mongoose = require("mongoose");
+mongoose.Promise = require("bluebird");
 
-    this.handleUserClick = this.handleUserClick.bind(this);
-  }
+const async = require("async");
 
-  componentDidMount() {
-    this.handleUserListChange();
-  }
+const express = require("express");
+const app = express();
 
-  handleUserListChange() {
-    axios.get("/user/list")
-      .then((response) => {
-        this.setState({
-          users: response.data
+// Load the Mongoose schema for User, Photo, and SchemaInfo
+const User = require("./schema/user.js");
+const Photo = require("./schema/photo.js");
+const SchemaInfo = require("./schema/schemaInfo.js");
+
+// XXX - Your submission should work without this line. Comment out or delete
+// this line for tests and before submission!
+// const models = require("./modelData/photoApp.js").models; // Keeping this for context, though it's unused
+mongoose.set("strictQuery", false);
+mongoose.connect("mongodb://127.0.0.1/project6", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+// We have the express static module
+// (http://expressjs.com/en/starter/static-files.html) do all the work for us.
+app.use(express.static(__dirname));
+
+app.get("/", function (request, response) {
+  response.send("Simple web server of files from " + __dirname);
+});
+
+/**
+ * Use express to handle argument passing in the URL. This .get will cause
+ * express to accept URLs with /test/<something> and return the something in
+ * request.params.p1.
+ * 
+ * If implement the get as follows:
+ * /test        - Returns the SchemaInfo object of the database in JSON format.
+ *                This is good for testing connectivity with MongoDB.
+ * /test/info   - Same as /test.
+ * /test/counts - Returns an object with the counts of the different collections
+ *                in JSON format.
+ */
+app.get("/test/:p1?", function (request, response) {
+  // Express parses the ":p1" from the URL and returns it in the request.params
+  // objects.
+  console.log("/test called with param1 = ", request.params.p1);
+
+  const param = request.params.p1 || "info";
+
+  if (param === "info") {
+    // Fetch the SchemaInfo. There should only one of them. The query of {} will
+    // match it.
+    SchemaInfo.find({}, function (err, info) {
+      if (err) {
+        // Query returned an error. We pass it back to the browser with an
+        // Internal Service Error (500) error code.
+        console.error("Error in /test/info:", err);
+        response.status(500).send(JSON.stringify(err));
+        return;
+      }
+      if (info.length === 0) {
+        // Query didn't return an error but didn't find the SchemaInfo object -
+        // This is also an internal error return.
+        response.status(500).send("Missing SchemaInfo");
+        return;
+      }
+
+      // We got the object - return it in JSON format.
+      console.log("SchemaInfo", info[0]);
+      response.json(info[0]);  // Using response.json for consistent return
+    });
+  } else if (param === "counts") {
+    // In order to return the counts of all the collections we need to do an
+    // async call to each collection. That is tricky to do so we use the async
+    // package to do the work. We put the collections into an array and use async.each
+    // to do each .count() query.
+    const collections = [
+      { name: "user", collection: User },
+      { name: "photo", collection: Photo },
+      { name: "schemaInfo", collection: SchemaInfo },
+    ];
+    async.each(
+      collections,
+      function (col, done_callback) {
+        col.collection.countDocuments({}, function (err, count) {
+          col.count = count;
+          done_callback(err);
         });
-      })
-      .catch(error => {
-        console.error("There was an error fetching the user list!", error);
-      });
-  }
-
-  handleUserClick(userId) {
-    console.log(`Navigating to user: ${userId}`);
-    
-    const { users } = this.state;
-    const userExists = users.some(user => user._id === userId);
-    
-    if (userExists) {
-      window.location.href = `/photo-share.html#/users/${userId}`;
-      window.location.reload();
-    } else {
-      console.error('User not found in the list.');
-    }
-  }
-
-  render() {
-    return this.state.users.length > 0 ? (
-      <div>
-        <List component="nav">
-          {this.state.users.map(user => (
-            <ListItemButton
-              selected={this.state.user_id === user._id}
-              key={user._id}
-              divider={true}
-              component="a"
-              href={"#/users/" + user._id}
-            >
-              <ListItemText primary={`${user.first_name} ${user.last_name}`} />
-            </ListItemButton>
-          ))}
-        </List>
-      </div>
-    ) : (
-      <div>No users found.</div>
+      },
+      function (err) {
+        if (err) {
+          response.status(500).send(JSON.stringify(err));
+        } else {
+          const obj = {};
+          for (let i = 0; i < collections.length; i++) {
+            obj[collections[i].name] = collections[i].count;
+          }
+          response.json(obj);  // Using response.json for consistent return
+        }
+      }
     );
+  } else {
+    // If we don't understand the parameter we return a (Bad Parameter) (400)
+    // status.
+    response.status(400).send("Bad param " + param);
   }
-}
+});
 
-export default UserList;
+/**
+ * URL /user/list - Returns all the User objects.
+ */
+app.get("/user/list", function (request, response) {
+
+  // Get all users
+  User.find({}, function (err, users) {
+    if (err) {
+      console.error("Error in /user/list:", err);
+      response.status(400).send(JSON.stringify(err));
+      return;
+    }
+    if (users.length === 0) {
+      response.status(400).send("Missing Users Info");
+      return;
+    }
+
+    // Convert user objects to objects with only id, first_name, and last_name
+    const userList = users.map(user => ({
+      _id: user._id.toString(),  // Convert ObjectId to string explicitly if necessary
+      first_name: user.first_name,
+      last_name: user.last_name
+    }));
+    response.json(userList);  // Using response.json for consistent return
+  });
+});
+
+/**
+ * URL /user/:id - Returns the information for User (id).
+ */
+app.get("/user/:id", function (request, response) {
+  const id = request.params.id;
+
+  // Corrected to use findById for a more appropriate query by _id
+  User.findById(id, { __v: 0 }, function (err, user) {
+    if (err) {
+      // Handle errors from the database
+      console.error("Error in /user/:id", err);
+      response.status(400).send(JSON.stringify(err));
+      return;
+    }
+    if (!user) {
+      // No user found with the provided id
+      response.status(404).send("User not found");
+      return;
+    }
+    // Successfully found the user, return the user data
+    response.json(user);  // Using response.json for consistent return
+  });
+});
+
+/**
+ * URL /photosOfUser/:id - Returns the Photos for User (id).
+ */
+app.get("/photosOfUser/:id", function (request, response) {
+  const userId = request.params.id;
+  if (!mongoose.Types.ObjectId.isValid(userId)) {
+    return response.status(400).send("Invalid user ID format.");
+  }
+
+  Photo.aggregate([
+    { $match: { user_id: mongoose.Types.ObjectId(userId) } },
+    { $addFields: { comments: { $ifNull: ["$comments", []] } } },
+    { $lookup: {
+        from: "users",
+        localField: "comments.user_id",
+        foreignField: "_id",
+        as: "commentUsers"
+      } },
+    { $addFields: {
+        comments: {
+          $map: {
+            input: "$comments",
+            as: "comment",
+            in: {
+              $mergeObjects: [
+                "$$comment",
+                { user: {
+                    $first: {
+                      $filter: {
+                        input: "$commentUsers",
+                        as: "commentUser",
+                        cond: { $eq: ["$$commentUser._id", "$$comment.user_id"] }
+                      }
+                    }
+                  } }
+              ]
+            }
+          }
+        }
+      } },
+    { $project: {
+        commentUsers: 0,
+        __v: 0,
+        "comments.__v": 0,
+        "comments.user_id": 0,
+        "comments.user.description": 0,
+        "comments.user.location": 0,
+        "comments.user.occupation": 0,
+        "comments.user.__v": 0
+      } }
+  ]).exec((err, photos) => {
+    if (err) {
+      console.error("Error retrieving photos for user:", err);
+      return response.status(500).send(JSON.stringify(err));
+    }
+    if (photos.length === 0) {
+      return response.status(404).send("No photos found for this user.");
+    }
+    return response.json(photos);
+  });
+
+  
+  return null; 
+});
+
+
+const server = app.listen(3000, function () {
+  const port = server.address().port;
+  console.log(
+    "Listening at http://localhost:" +
+      port +
+      " exporting the directory " +
+      __dirname
+  );
+});
