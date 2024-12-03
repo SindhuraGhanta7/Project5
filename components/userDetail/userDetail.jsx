40233b4b58480b58da13a3b4b07b5ff2b37cbf9b
shipra-99 <shipra@charlotte.edu>
2024-12-03 15:03:09 -0500

    extended page changes

--- components/userDetail/userDetail.jsx
+++ components/userDetail/userDetail.jsx
@@ -1,182 +1,106 @@
-import React from 'react';
-import {
-  Typography, 
-  Grid, 
-  Paper,
-  Dialog,
-  DialogActions,
-  DialogContent,
-  DialogContentText,
-  DialogTitle,
-  Divider,
-  Button
-} from '@mui/material';
-import { Container } from '@mui/system';
-import { styled } from '@mui/material/styles';
-import { Link } from 'react-router-dom';
-import './userDetail.css';
-import axios from 'axios';
-import { withRouter } from "react-router";
+import { Button, Typography, Box, Avatar } from "@mui/material";
+import { useHistory } from "react-router-dom";
+import "./userDetail.css";
+import ChatBubbleOutlineIcon from "@mui/icons-material/ChatBubbleOutline";
+import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
 
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
-/**
- * Define UserDetail, a React component of project #5
- */
-class UserDetail extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      user: null,
-      isSettingsDialogOpen: false,
-      isDeleteDialogOpen: false
-    };
-  }
-
-  componentDidMount() {
-    this.getUserData();
-  }
-
-  componentDidUpdate(prevProps){
-    if (this.props.match.params.userId !== prevProps.match.params.userId){
-      this.getUserData();
+function UserDetail(props) {
+  function stringToColor(string) {
+    let hash = 0;
+    let i;
+    /* eslint-disable no-bitwise */
+    for (i = 0; i < string.length; i += 1) {
+      hash = string.charCodeAt(i) + ((hash << 5) - hash);
     }
+    let color = "#";
+    for (i = 0; i < 3; i += 1) {
+      const value = (hash >> (i * 8)) & 0xff;
+      color += `00${value.toString(16)}`.slice(-2);
+    }
+    /* eslint-enable no-bitwise */
+    return color;
   }
+  const [userData, setUserData] = useState();
+  const [recentPhoto, setRecentPhoto] = useState();
+  const [mostComments, setMostComments] = useState();
+  const history = useHistory();
+  useEffect(() => {
+    axios
+      .get("http://localhost:3000/user/" + props.match.params.userId)
+      .then((res) => {
+        setUserData(res.data);
+        props.setTitle(`${res?.data?.first_name} ${res?.data?.last_name}`);
+      })
+      .catch((err) => console.log(err));
+    axios
+      .get("http://localhost:3000/photos/latest/" + props.match.params.userId)
+      .then((res) => {
+        setRecentPhoto(res.data);
+      })
+      .catch((err) => console.log(err));
+    axios
+      .get("http://localhost:3000/photos/most-comments/" + props.match.params.userId)
+      .then((res) => {
+        setMostComments(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, [props.match.params.userId]);
 
-  getUserData() {
-    axios.get("http://localhost:3000/user/" + this.props.match.params.userId).then((response) => {
-      this.setState({ user: response.data });
-    });
-  }
-
-  handleSettingsDialogOpen = () => {
-    this.setState({isSettingsDialogOpen: true});
-  };
-
-  handleSettingsDialogClose = () => {
-    this.setState({isSettingsDialogOpen: false});
-  };
-
-  handleDeleteDialogOpen = () => {
-    this.setState({isDeleteDialogOpen: true});
-  };
-
-  handleDeleteDialogClose = () => {
-    this.setState({isDeleteDialogOpen: false});
-  };
-
-  handleSettingsToDeleteDialog = () => {
-    this.setState({isSettingsDialogOpen: false});
-    this.setState({isDeleteDialogOpen: true});
-  };
-
-  handleDeleteToSettingsDialog = () => {
-    this.setState({isDeleteDialogOpen: false});
-    this.setState({isSettingsDialogOpen: true});
-  };
-
-  handleAccountDelete = e => {
-    e.preventDefault();
-    axios.delete("http://localhost:3000/deleteUser/" + this.state.user._id).then(() => {
-      localStorage.removeItem("uid");
-      this.props.setLogout();
-      if (this.props.location.pathname.split("/")[1] !== "login-register"){
-        this.props.history.push("/login-register");
-      }
-    });
-  };
- 
-  render() {
-    const Item = styled(Paper)(({ theme }) => ({
-      backgroundColor: theme.palette.mode === 'dark' ? '' : '#fff',
-      ...theme.typography.body2,
-      padding: theme.spacing(1),
-      textAlign: 'center',
-      color:'#004643'
-    }));
-
-
-    return (
-      <div>
-        {this.state.user && (
-          <Container>
-            <Grid container spacing={2}>
-              <Grid item xs={12}>
-                <Item>
-                  <Typography variant='h4' className="details-title-edit">
-                    {this.state.user.first_name + " " + this.state.user.last_name}
-                  </Typography>
-                </Item>
-              </Grid>
-              <Grid item xs={4} lg={12}>
-                <Item>
-                  <Typography className="details-list-edit">
-                    Location: {this.state.user.location}
-                  </Typography>
-                </Item>
-              </Grid>
-              <Grid item xs={4} lg={12}>
-                <Item>
-                  <Typography className="details-list-edit">
-                      Description: {this.state.user.description}
-                  </Typography>
-                </Item>
-              </Grid>
-              <Grid item xs={4} lg={12}>
-                <Item>
-                  <Typography className="details-list-edit">
-                      Occupation: {this.state.user.occupation}
-                  </Typography>
-                </Item>
-              </Grid>
-              <Grid item xs={12}>
-                <Item>
-                  <Typography className="user-photos-link">
-                    <Link className="user-photos-link-font" to={"/photos/" + this.state.user._id}>Go to user photos</Link>
-                  </Typography>
-                </Item>
-              </Grid>
-              {
-                localStorage.getItem("uid") === this.state.user._id && (
-                  <Grid item xs={12}>
-                    <Item>
-                      <Button className="user-settings-button" onClick={this.handleSettingsDialogOpen}>Settings</Button>
-                    </Item>
-                  </Grid>
-                )
-              }
-            </Grid>
-          </Container>
+  console.log(recentPhoto, mostComments, "RECENT PHOTO");
+  return (
+    <Box>
+      <Avatar sx={{ bgcolor: stringToColor(`${userData?.first_name?.[0]}${userData?.last_name?.[0]}`), width: 100, height: 100, fontSize: 40 }}>
+        {userData?.first_name?.[0]}
+        {userData?.last_name?.[0]}
+      </Avatar>
+      <Typography fontSize={30}>{userData?.first_name}&apos;s Profile</Typography>
+      <Typography>First Name: {userData?.first_name}</Typography>
+      <Typography>Last Name: {userData?.last_name}</Typography>
+      <Typography>Occupation: {userData?.occupation}</Typography>
+      <Typography>Location: {userData?.location}</Typography>
+      <Typography>Description: {userData?.description}</Typography>
+      {userData?._id && (
+        <Button variant="contained" onClick={() => history.push("/photos/" + userData._id)} sx={{ marginTop: "12px" }}>
+          See {userData?.first_name} {userData?.last_name}&apos;s photos
+        </Button>
+      )}
+      <Box sx={{ width: "100%", borderTop: "1px solid gray", paddingTop: "20px", display: "flex", flexDirection: "row", marginTop: "20px" }}>
+        {recentPhoto && (
+          <Box sx={{ width: "50%", cursor: "pointer" }} onClick={() => history.push("/photos/" + props.match.params.userId + "?imageId=" + (recentPhoto?.file_name || ""))}>
+            <Box sx={{ width: "400px", height: "100%", display: "flex", flexDirection: "column" }}>
+              <Typography>Recent Photo</Typography>
+              <Box sx={{ display: "flex", flex: 1, alignItems: "center", justifyContent: "center", backgroundColor: "black" }}>
+                <img src={`../../images/${recentPhoto?.file_name}`} alt="recent" className="main-image" />
+              </Box>
+            </Box>
+            <Box display="flex" flexDirection="row" alignItems="center" gap={1} marginTop={2}>
+              <ChatBubbleOutlineIcon /> <Typography sx={{ marginRight: 3 }}>{recentPhoto?.comments?.length}</Typography>
+              <FavoriteBorderIcon /> <Typography>{recentPhoto?.liked_by?.length || 0}</Typography>
+            </Box>
+          </Box>
         )}
-        <Dialog className="settings-dialog" open={this.state.isSettingsDialogOpen} onClose={this.handleSettingsDialogClose}>
-          <DialogTitle>Settings</DialogTitle>
-          <Divider/>
-          <DialogContent className="delete-container">
-            <Typography className="delete-text">Delete Account</Typography>
-            <Button className="delete-button" onClick={this.handleSettingsToDeleteDialog}>Delete</Button>
-          </DialogContent>
-          <Divider/>
-          <DialogActions>
-            <Button onClick={this.handleSettingsDialogClose}>Back</Button>
-          </DialogActions>
-        </Dialog>
-
-        <Dialog className="delete-dialog" open={this.state.isDeleteDialogOpen} onClose={this.handleDeleteDialogClose}>
-          <DialogTitle>Delete Account?</DialogTitle>
-          <DialogContent>
-            <DialogContentText>
-              Are you sure you want to delete your account?
-            </DialogContentText>
-          </DialogContent>
-          <DialogActions>
-            <Button onClick={this.handleDeleteToSettingsDialog}>No</Button>
-            <Button onClick={this.handleAccountDelete}>Yes</Button>
-          </DialogActions>
-        </Dialog>
-      </div>
-    );
-  }
+        {mostComments && (
+          <Box sx={{ width: "50%", cursor: "pointer" }} onClick={() => history.push("/photos/" + props.match.params.userId + "?imageId=" + (mostComments?.file_name || ""))}>
+            <Box sx={{ width: "400px", height: "100%", display: "flex", flexDirection: "column" }}>
+              <Typography>Most Comments</Typography>
+              <Box sx={{ display: "flex", flex: 1, alignItems: "center", justifyContent: "center", backgroundColor: "black" }}>
+                <img src={`../../images/${mostComments?.file_name}`} alt="most comments" className="main-image" />
+              </Box>
+            </Box>
+            <Box display="flex" flexDirection="row" alignItems="center" gap={1} marginTop={2}>
+              <ChatBubbleOutlineIcon /> <Typography sx={{ marginRight: 3 }}>{mostComments?.comments?.length}</Typography>
+              <FavoriteBorderIcon /> <Typography>{mostComments?.liked_by?.length || 0}</Typography>
+            </Box>
+          </Box>
+        )}
+      </Box>
+    </Box>
+  );
 }
-const userDetailComponent = withRouter(UserDetail);
 
-export default userDetailComponent;
-
+export default UserDetail;
